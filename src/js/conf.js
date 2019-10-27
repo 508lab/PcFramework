9e782bfb008b317635c3b36d77789621cd6c374d
schizobulia <2833324528@qq.com>
2019-10-27 15:41:22 +0800

    add news loading

--- src/js/conf.js
+++ src/js/conf.js
@@ -1,8 +1,10 @@
 const Application = {
-    APIURL: "http://localhost:8080/v1/api",
-    // APIURL: "https://api.dongkji.com/api/v1/api/",
-    // HASHPATH: '/pc',
-    HASHPATH: '',
+    // APIURL: "http://localhost:8080/v1/api",
+    // HASHPATH: '',
+
+    APIURL: "https://api.dongkji.com/api/v1/api/",
+    HASHPATH: '/pc',
+
 
     httpGet: function (uri, callback) {
         $.ajax({
@@ -11,14 +13,14 @@ const Application = {
             success: function (data) {
                 let res = JSON.parse(data);
                 if (res.status === 200) {
-                    callback(res.data);
+                    callback(res.data, res);
                 } else {
                     callback(res.status);
                     alert('服务器异常');
                 }
             },
             error: function (err) {
-                callback(err);
+                callback('err', err);
             }
         })
     }
--- src/js/news.js
+++ src/js/news.js
@@ -1,13 +1,74 @@
+var scrollId = '';
+
 init();
 
 function init() {
     $('body').mLoading("show");
     initView();
+    loadingMore();
+    search();
+}
+
+function loadingMore() {
+    $("#loading").click(function () {
+        $('body').mLoading("show");
+        Application.httpGet('/news/scroll/' + scrollId, function (data, res) {
+            if (data === 'err' && res.statusText === 'error') {
+                alert('缓存数据过期...将刷新页面');
+                window.location.reload();
+                return;
+            }
+            scrollId = res.scrollid;
+            let html = $('.news').html();
+            data.map(function (ele) {
+                html += fullListView(JSON.parse(ele));
+            });
+            $('.news').html(html);
+            $('body').mLoading("hide");
+        });
+    })
+}
+
+function search() {
+    $('#search').click(function () {
+        searchByQ();
+    })
+
+    $('#q').keydown(function (e) {
+        if (e.keyCode === 13) {
+            searchByQ();
+        }
+    })
+}
+
+
+function searchByQ() {
+    let q = $('#q').val();
+    if (q && q !== '') {
+        queryByQ(q);
+    } else {
+        queryByQ('*');
+        alert("输入不能为空！");
+    }
+}
+
+function queryByQ(q) {
+    $('body').mLoading("show");
+    Application.httpGet('/news/search/' + q, function (data, res) {
+        scrollId = res.scrollid; 
+        let html = '';
+        data.map(function (ele) {
+            html += fullListView(JSON.parse(ele));
+        });
+        $('.news').html(html);
+        $('body').mLoading("hide");
+    });
 }
 
 function initView() {
     $('header nav li.nnews').addClass('active');
-    Application.httpGet('/news', function (data) {
+    Application.httpGet('/news', function (data, res) {
+        scrollId = res.scrollid;
         let html = '';
         data.map(function (ele) {
             html += fullListView(JSON.parse(ele));
